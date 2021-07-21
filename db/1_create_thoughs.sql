14cc49297c6ab481a4b852f583002dc39c21c235
Scott <scott-a-ryan@hotmail.co.uk>
2021-07-21 11:46:18 +0100

    linking nicely!

--- db/1_create_thoughs.sql
+++ db/1_create_thoughs.sql
@@ -4,5 +4,5 @@ CREATE TABLE thoughts(
     id serial,
     title varchar(100) NOT NULL,
     pseudonym varchar(100) NOT NULL,
-    post_body varchar(500) NOT NULL
+    body varchar(500) NOT NULL
 );
